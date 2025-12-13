72141a3bc9507253d8133130b977879eff32e5c4
coxlaurel <coxlaurel@gmail.com>
2025-12-13 18:20:26 -0500

    rest note implemented, note is 4-bit

--- hdl/pwm.sv
+++ hdl/pwm.sv
@@ -13,9 +13,13 @@ module pwm_generator (
 
     // Implement counter for timing transition in PWM output signal
     always_ff @(posedge clk) begin
-        if (pwm_count == pwm_interval - 1) begin
+        if (pwm_interval == 16'd0) begin
+            pwm_count <= 16'd0;
+            wave <= 1'b0;
+        end
+        else if ((pwm_count == pwm_interval - 1)) begin
             pwm_count <= 0;
-            wave = ~wave;
+            wave <= ~wave;
         end
         else begin
             pwm_count <= pwm_count + 1;
@@ -33,18 +37,20 @@ endmodule
  */
 module pwm_decoder (
     input logic clk,
-    input logic [2:0] note,
+    input logic [3:0] note,
     output logic [15:0] pwm_interval
 );
 
-    localparam [2:0] NOTE_C4 = 3'b000;
-    localparam [2:0] NOTE_D4 = 3'b001;
-    localparam [2:0] NOTE_E4 = 3'b011;
-    localparam [2:0] NOTE_F4 = 3'b100;
-    localparam [2:0] NOTE_G4 = 3'b110;
-    localparam [2:0] NOTE_A4 = 3'b101;
-    localparam [2:0] NOTE_B4 = 3'b010;
-    localparam [2:0] NOTE_C5 = 3'b111;
+    localparam [3:0] NOTE_C4 = 4'b0000;
+    localparam [3:0] NOTE_D4 = 4'b0001;
+    localparam [3:0] NOTE_E4 = 4'b0011;
+    localparam [3:0] NOTE_F4 = 4'b1000;
+    localparam [3:0] NOTE_G4 = 4'b0110;
+    localparam [3:0] NOTE_A4 = 4'b0101;
+    localparam [3:0] NOTE_B4 = 4'b0010;
+    localparam [3:0] NOTE_C5 = 4'b0111;
+    localparam [3:0] NOTE_REST = 4'b1111;
+
 
     logic [15:0] interval;
 
@@ -59,10 +65,9 @@ module pwm_decoder (
             NOTE_A4: pwm_interval = 16'd13636;
             NOTE_B4: pwm_interval = 16'd12148;
             NOTE_C5: pwm_interval = 16'd11471;
+            NOTE_REST: pwm_interval = 16'd0;
             default: pwm_interval = 16'd5000;
         endcase
     end
-
-    // assign pwm_interval = interval;
 
 endmodule
