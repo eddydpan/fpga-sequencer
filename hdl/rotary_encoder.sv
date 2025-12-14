527b6a35bc6bff295e1003f59d9200b280f0d040
eddydpan <eddydotpan@gmail.com>
2025-12-13 23:34:40 -0500

    Integrate rotary encoder

--- hdl/rotary_encoder.sv
+++ hdl/rotary_encoder.sv
@@ -6,23 +6,23 @@ module rotary_encoder (
         input logic         signal_a,
         input logic         signal_b,
         output logic        button_pressed, // outputs
-        output logic [2:0]  rotary_position // 3-bits for 8 notes. we assume that state 7 --> state 0
+        output logic [3:0]  rotary_position // 4-bits for 9 notes. we assume that state 8 --> state 0
     );
 
-    logic [2:0] counter, next_counter = 3'b000;
+    logic [3:0] counter, next_counter = 4'b0001;
 
 
     always_ff @(posedge signal_a) begin 
         if (signal_b == 1'b0) begin
-            if (counter == 3'b111) // wrap from state 7 --> state 0
-                next_counter = 3'b000;
+            if (counter == 4'b1000) // wrap from state 8 --> state 0
+                next_counter = 4'b0001;
             else
-                next_counter = counter + 3'b001;
+                next_counter = counter + 4'b0001;
         end else begin
-            if (counter == 3'b000) // wrap from state 0 --> state 7
-                    next_counter = 3'b111;
+            if (counter == 4'b0001) // wrap from state 0 --> state 8
+                    next_counter = 4'b1000;
                 else
-                    next_counter = counter - 3'b001;
+                    next_counter = counter - 4'b0001;
         end
 
     end
