ffb94da3308f40b65b67964e4ce794888d8fca83
eddydpan <eddydotpan@gmail.com>
2025-12-13 19:09:09 -0500

    Integrating pwm, needs debugging

--- hdl/audio_controller.sv
+++ hdl/audio_controller.sv
@@ -1,11 +1,22 @@
+`include "pwm.sv"
+
 module audio_controller #(
     parameter NUM_BEATS = 16
 )(
     input logic clk,
-    input logic[48:0] beats, // TODO: dynamic buffer size based on NUM_BEATS
+    input logic[NUM_BEATS*4-1:0] beats, // TODO: dynamic buffer size based on NUM_BEATS
     output logic [$clog2(NUM_BEATS)-1:0] beat_count,
-    output logic pwm_out
+    output logic pwm_out 
 );
+
+    initial begin
+        beat_count = 0;
+    end
+    
+    localparam [31:0] beat_clock_interval = 12_000_000 / NUM_BEATS;
+    logic [3:0] pitch;
+    logic [15:0] pwm_interval;
+
     pwm_decoder u_pwm_decoder (
         .clk(clk),
         .note(pitch),
@@ -19,13 +30,16 @@ module audio_controller #(
     );
     always @(posedge clk) begin
         // Clock divider based off beats
-        if (beat_count == NUM_BEATS - 1) begin
-            beat_count <= 0;
-        end else begin
+        if (clk % (beat_clock_interval) == 0) begin
             beat_count <= beat_count + 1;
         end
+        // if (beat_count == NUM_BEATS - 1) begin
+        //     beat_count <= 0;
+        // end else begin
+        //     beat_count <= beat_count + 1;
+        // end
 
-        pitch <= beats[beat_count*3 +: 3];
+        pitch <= beats[beat_count*4 +: 4];
 
     end
 
